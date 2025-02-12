8f1d0ae161047762d4976ddcee5d44485a9e09e4
Debora Barolin <barolindebora224@gmail.com>
2025-02-12 16:19:41 -0300

    carpeta fs

--- path_ejemplo/index.mjs
+++ path_ejemplo/index.mjs
@@ -1,6 +1,6 @@
 import path from 'path'; 
 // Definir una ruta de archivo de ejemplo
-const filePath='/.path_ejemplo/index.mjs'; 
+const filePath='/data/example.txt';
 
 // Obtener el directorio base
  const dirName=path.dirname(filePath); 
